a3111a8c81e21e9f4db75d4ec5a6ab2e53a90ccd
Oliveira Matsumoto <tsmo@gft.com>
2017-08-22 17:30:01 -0300

    retirada do componente de fotos e galeria

--- www/js/Controller/controllers.js
+++ www/js/Controller/controllers.js
@@ -100,7 +100,6 @@ angular.module('starter.controllers', ['ionic','ngMaterial', 'ngMessages', 'mate
 
 
 .factory('paginacao', function($http){
-
     return {
 		indexAtual: 0
 	};
@@ -152,202 +151,113 @@ angular.module('starter.controllers', ['ionic','ngMaterial', 'ngMessages', 'mate
 
 		$scope.insertEnderecoVist = function() {
       if ($scope.enderecoVistForm.$valid) {
-
-				console.log($scope.enderecoVist);
 				dados.categoria = $scope.enderecoVist.categoria;
-
-        localStorage.setItem("enderecoVist", $scope.enderecoVist);
+        $scope.enderecoFinal = JSON.stringify($scope.enderecoVist);
+        localStorage.setItem("enderecoVist", $scope.enderecoFinal);
+        console.log("localStorage endereço: ",localStorage.enderecoVist);
         window.location.href = "#/gps";
-
-				// dados.insertEnderecoVist($scope.enderecoVist)
-				// 	.success(function (data, status) {
-				// 		console.log("cadastrado!");
-				// 		 window.location.href = "#/gps";
-				// 	})
-				// 	.error(function (data, status, headers, config) {
-				//        	console.log(data,  status);
-				//        	console.log("deu erro");
-				// 	});
-
 			}else{
 				alertaSegurado();
 			}
 		}
-
-
 	this.dadosUsuario = dados.usuario;
-
-
 
 })
 
 
-
-
-
 .controller('checkboxCtrl', function($scope, $ionicPlatform, dados, paginacao, $location) {
 
-
 	$ionicPlatform.ready(function() {
+  	$scope.rotasSalvas = JSON.parse(localStorage.rotas);
 
-
-		// $scope.dadosRotas = ({
-		// 	"idVistoria": dados.usuario.idVistoria
-		// });
-    //
-		// dados.buscaRotas($scope.dadosRotas)
-		// 	.success(function (data, status) {
-				$scope.rotasSalvasString = localStorage.rotas;
-				$scope.rotasSalvas = JSON.parse($scope.rotasSalvasString);
-				$scope.indexAtual = paginacao.indexAtual;
-				$scope.nextPage = $scope.rotasSalvas[$scope.indexAtual];
-
-			// })
-			// .error(function (data, status, headers, config) {
-			// 	console.log(data,  status);
-			// 	console.log("update itens erro");
-			// });
-
+    console.log("carregando rotas localStorage:", $scope.rotasSalvas);
+  	$scope.indexAtual = paginacao.indexAtual;
+  	$scope.nextPage = $scope.rotasSalvas[$scope.indexAtual];
+    console.log("proxima pagina",$scope.nextPage);
 	})
 
-
-
 	$scope.addIndex = function(){
-		console.log($scope.rotasSalvas.length);
-
 		if(paginacao.indexAtual == $scope.rotasSalvas.length){
 			$location.path("/resumo");
 		}
 		else{
 			paginacao.indexAtual = paginacao.indexAtual + 1;
-
 			console.log("index atual:" + paginacao.indexAtual);
-
 			$location.path($scope.nextPage);
 		}
-
-
 	}
 
-
-
-
-
-
 })
-
-
 
 .controller('alteraItensCtrl', function($scope, $http, dados, $ionicPopup){
 
 	$scope.dadosUsuario = dados.usuario;
-
-
 	$scope.alteraItens = function(){
-
-
-		function alertaItens() {
-			var alertPopup = $ionicPopup.alert({
-			  title: 'Falha ao confirmar dados da residência!',
-			 template: 'Por favor, preencha os campos obrigatórios corretamente!'
-			})
-		}
+	function alertaItens() {
+		var alertPopup = $ionicPopup.alert({
+		  title: 'Falha ao confirmar dados da residência!',
+		 template: 'Por favor, preencha os campos obrigatórios corretamente!'
+		})
+	}
 
 		if ($scope.itensVistForm.$valid) {
 
-				$scope.selecionados = [];
+			$scope.selecionados = [];
 
-				if($scope.itens.chk_camera != 2){
-					$scope.selecionados.push("/fotoCamera")
-				}
-				if($scope.itens.chk_geladeira != 2){
-					$scope.selecionados.push("/fotoGeladeira")
-				}
-				if($scope.itens.chk_fogao != 2){
-					$scope.selecionados.push("/fotoFogao")
-				}
-				if($scope.itens.chk_microondas != 2){
-					$scope.selecionados.push("/fotoMicroondas")
-				}
-				if($scope.itens.chk_maquinaLavar != 2){
-					$scope.selecionados.push("/fotoMaquinaLavar")
-				}
-				 if($scope.itens.chk_secadoura != 2){
-					$scope.selecionados.push("/fotoSecadoura")
-				}
-				 if($scope.itens.chk_computador != 2){
-					$scope.selecionados.push("/fotoComputador")
-				}
-				 if($scope.itens.chk_notebook != 2){
-					$scope.selecionados.push("/fotoNotebook")
-				}
-				 if($scope.itens.chk_filmadora != 2){
-					$scope.selecionados.push("/fotoFilmadora")
-				}
-				 if($scope.itens.chk_cameraFotog != 2){
-					$scope.selecionados.push("/fotoCameraFotografica")
-				}
-				 if($scope.itens.chk_televisao != 2){
-					$scope.selecionados.push("/fotoTelevisao")
-				}
-				 if($scope.itens.chk_homeTheater != 2){
-					$scope.selecionados.push("/fotoHomeTheater")
-				}
-				 if($scope.itens.chk_arCondic != 2){
-					$scope.selecionados.push("/fotoArCondicionado")
-				}
-				 if($scope.itens.chk_umidificador != 2){
-					$scope.selecionados.push("/fotoUmidificador")
-				}
-				 if($scope.itens.chk_aquecedor != 2){
-					$scope.selecionados.push("/fotoAquecedor")
-				}
+			if($scope.itens.chk_camera != 2){
+				$scope.selecionados.push("/fotoCamera")
+			}
+			if($scope.itens.chk_geladeira != 2){
+				$scope.selecionados.push("/fotoGeladeira")
+			}
+			if($scope.itens.chk_fogao != 2){
+				$scope.selecionados.push("/fotoFogao")
+			}
+			if($scope.itens.chk_microondas != 2){
+				$scope.selecionados.push("/fotoMicroondas")
+			}
+			if($scope.itens.chk_maquinaLavar != 2){
+				$scope.selecionados.push("/fotoMaquinaLavar")
+			}
+			 if($scope.itens.chk_secadoura != 2){
+				$scope.selecionados.push("/fotoSecadoura")
+			}
+			 if($scope.itens.chk_computador != 2){
+				$scope.selecionados.push("/fotoComputador")
+			}
+			 if($scope.itens.chk_notebook != 2){
+				$scope.selecionados.push("/fotoNotebook")
+			}
+			 if($scope.itens.chk_filmadora != 2){
+				$scope.selecionados.push("/fotoFilmadora")
+			}
+			 if($scope.itens.chk_cameraFotog != 2){
+				$scope.selecionados.push("/fotoCameraFotografica")
+			}
+			 if($scope.itens.chk_televisao != 2){
+				$scope.selecionados.push("/fotoTelevisao")
+			}
+			 if($scope.itens.chk_homeTheater != 2){
+				$scope.selecionados.push("/fotoHomeTheater")
+			}
+			 if($scope.itens.chk_arCondic != 2){
+				$scope.selecionados.push("/fotoArCondicionado")
+			}
+			 if($scope.itens.chk_umidificador != 2){
+				$scope.selecionados.push("/fotoUmidificador")
+			}
+			 if($scope.itens.chk_aquecedor != 2){
+				$scope.selecionados.push("/fotoAquecedor")
+			}
 
 				// CONVERTE A ARRAY NUMA STRING PARA PODER SALVAR NO BANCO
 				$scope.dadoFinal = JSON.stringify($scope.selecionados);
 
 				// IMPORTANTE PARA CONVERTER A STRING DE NOVO EM ARRAY
 				// $scope.dadoErrado = JSON.parse($scope.dadoFinal);
-
-
-				$scope.dadosRotas = ({
-					"idVistoria": dados.usuario.idVistoria,
-					"rotas": $scope.dadoFinal
-
-				});
-
-
-				console.log("itens: ", $scope.itens);
-				console.log("rotas:", $scope.dadosRotas);
-
-        localStorage.setItem("rotas", $scope.dadosRotas)
+        localStorage.setItem("rotas", $scope.dadoFinal)
+        console.log("rotas localStorage:", localStorage.rotas);
         window.location.href = "#/fotoPorta";
-
-				// dados.updateItens($scope.itens)
-				// 	.success(function (data, status) {
-				// 		console.log(data);
-				// 		console.log("update itens");
-				// 		// window.location.href = "#/casa";
-        //
-				// 	})
-				// 	.error(function (data, status, headers, config) {
-				// 		console.log(data,  status);
-				// 		console.log("update itens erro");
-				// 	});
-        //
-				// dados.updateRotas($scope.dadosRotas)
-				// 	.success(function (data, status) {
-				// 		console.log(data);
-				// 		console.log("update rotas");
-				// 		window.location.href = "#/fotoPorta";
-        //
-				// 	})
-				// 	.error(function (data, status, headers, config) {
-				// 		console.log(data,  status);
-				// 		console.log("update rotas erro");
-        //
-        //
-				// 	});
 		}else{
 			alertaItens();
 		}
@@ -382,7 +292,6 @@ angular.module('starter.controllers', ['ionic','ngMaterial', 'ngMessages', 'mate
 
 	$scope.updateDadosUsuario = function(){
 		if ($scope.seguradoForm.$valid) {
-			console.log($scope.user);
       localStorage.setItem("foneResidencial", $scope.user.foneResidencial);
       localStorage.setItem("foneCelular", $scope.user.foneCelular);
       localStorage.setItem("email", $scope.user.email);
@@ -401,8 +310,9 @@ angular.module('starter.controllers', ['ionic','ngMaterial', 'ngMessages', 'mate
 			alertaSegurado();
 		}
 	}
+
   localStorage.setItem("nomeUsuario", 'Soraya Suzuki Tanikawa');
-  localStorage.setItem("dtNascimento", '1995-12-18');
+  localStorage.setItem("dtNascimento", '18/12/1985');
   localStorage.setItem("rg", '381435477');
   localStorage.setItem("orgaoEmissor", 'SSP');
   localStorage.setItem("logradouro_user", 'Alameda Rio Negro');
@@ -442,28 +352,30 @@ angular.module('starter.controllers', ['ionic','ngMaterial', 'ngMessages', 'mate
 		})
 	}
 
-	$scope.dadosUsuario = dados.usuario;
-	$scope.dadosVistoria = dados.usuario;
-
-	$scope.enderecoVist  = dados.usuario;
+	$scope.dadosUsuario = [];
+  $scope.dadosUsuario.nomeUsuario = localStorage.nomeUsuario;
+  $scope.dadosUsuario.cpf = localStorage.cpf;
+  $scope.dadosUsuario.email = localStorage.email;
+  $scope.dadosUsuario.foneResidencial = localStorage.foneResidencial;
+  $scope.dadosUsuario.foneCelular = localStorage.foneCelular;
 
 	$scope.updateVistoria = function(){
 
 			console.log($scope.dadosVistoria);
+      window.location.href = "#/obrigado";
 
-			dados.updateVistoria($scope.dadosVistoria)
-				.success(function (data, status) {
-					console.log(data);
-					window.location.href = "#/obrigado";
-					console.log("resumo ok");
-
-				})
-				.error(function (data, status, headers, config) {
-					console.log(data,  status);
-					console.log("deu erro");
-					alertaErro();
-				});
-		console.log("update Vistoria");
+			// dados.updateVistoria($scope.dadosVistoria)
+			// 	.success(function (data, status) {
+			// 		console.log(data);
+			// 		window.location.href = "#/obrigado";
+			// 		console.log("resumo ok");
+      //
+			// 	})
+			// 	.error(function (data, status, headers, config) {
+			// 		console.log(data,  status);
+			// 		console.log("deu erro");
+			// 		alertaErro();
+			// 	});
 	}
 
 })
@@ -480,7 +392,6 @@ angular.module('starter.controllers', ['ionic','ngMaterial', 'ngMessages', 'mate
   $scope.submitForm = function() {
     if ($scope.userForm.$valid) {
       console.log($scope.user);
-
 
       dados.validaLogin($scope.user).success(function (data, status, headers, config) {
         if(data !== "" ){
@@ -640,24 +551,24 @@ angular.module('starter.controllers', ['ionic','ngMaterial', 'ngMessages', 'mate
 
             var mapOptions = {
                 center: myLatlng,
-				draggable: false,
-				disableDoubleClickZoom: true,
-				streetViewControl: false,
+        				draggable: false,
+        				disableDoubleClickZoom: true,
+        				streetViewControl: false,
                 zoom: 16,
-				zoomControl: false,
-				panControl: false,
-				mapMaker: false,
-				keyboardShortcuts: false,
-				overviewMapControl: false,
-				mapTypeControl: false,
-				rotateControl: false,
-				scaleControl: false,
-				scrollwheel: false,
+        				zoomControl: false,
+        				panControl: false,
+        				mapMaker: false,
+        				keyboardShortcuts: false,
+        				overviewMapControl: false,
+        				mapTypeControl: false,
+        				rotateControl: false,
+        				scaleControl: false,
+        				scrollwheel: false,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
 
             var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-			marker = new google.maps.Marker({
+			          marker = new google.maps.Marker({
                 position: myLatlng,
                 map: map,
                 draggable:false
@@ -692,90 +603,47 @@ angular.module('starter.controllers', ['ionic','ngMaterial', 'ngMessages', 'mate
 
 
 
-.controller('fotosCtrl', function($scope, dados, $ionicPlatform, ImageUtil, FileUtil, $ionicModal, $ionicPopup) {
+.controller('fotosCtrl', function($scope, dados, $ionicPlatform, ImageUtil, FileUtil, $ionicModal, $ionicPopup, $cordovaCamera) {
 	$scope.imageCamera = undefined;
 
-	$scope.onClickC = function(){
-		$scope.imageCamera = undefined;
-		ImageUtil.getImage(ImageUtil.cameraOptions.CAMERA, function(imageData){
-			console.log("sucesso");
-			$scope.imageCamera = "data:image/jpeg;base64," + imageData;
-		},
-		function(err){
-			console.log(err);
-		});
+  function alertaTirarFoto() {
+	   var alertPopup = $ionicPopup.alert({
+		 title: 'Tire uma foto.',
+		 template: 'Neste momento será iniciado a câmera para o usuário tirar a foto.'
+	   })
+	}
 
+  function alertaEscolherFoto() {
+	   var alertPopup = $ionicPopup.alert({
+		 title: 'Escolha uma foto do album',
+		 template: 'Neste momento irá abrir a galeria para o usuário escolher a foto.'
+	   })
+	}
+
+	$scope.onClickC = function(){
+    alertaTirarFoto();
 	}
 
 	$scope.onClickG = function() {
-    $scope.imageCamera = undefined;
-		ImageUtil.getImage(ImageUtil.cameraOptions.GALLERY, function(imageData) {
-			$scope.imageCamera = "data:image/jpeg;base64,"+imageData;
-		},
-		function(err) {
-			console.log(err);
-		});
-	}
+    alertaEscolherFoto();
+  }
 
 	$scope.imagemBd = undefined;
 
 	$scope.buscaImagemBd = function() {
-		console.log($scope.folder);
-
-		$scope.dadosBuscaImagem = ({
-			"idVistoria": dados.usuario.idVistoria,
-			"tipoImagem": $scope.folder
-		});
-
-    if(localStorage.$scope.folder !== "" ) {
-      dados.imagem = localStorage.$scope.folder;
-      $scope.imagemBd = dados.imagem;
-      console.log($scope.imagemBd);
-      console.log("deu certo");
-    }
-    else {
-      console.log("sem retorno do bd");
-    }
-
-		// dados.buscaImagem($scope.dadosBuscaImagem)
-		// 	.success(function (data, status, headers, config) {
-		// 	console.log(data);
-		// 		if(data !== "" ) {
-		// 			dados.imagem = data.tipoImagem;
-		// 			$scope.imagemBd = dados.imagem;
-		// 			console.log($scope.imagemBd);
-		// 			console.log("deu certo");
-		// 		}
-		// 		else {
-		// 			console.log("sem retorno do bd");
-		// 		}
-		//         
-		//     }).error(function (data, status, headers, config) {
-		//        	console.log(data,  status);
-		//        	console.log("deu erro");
-    //
-		//     });
+		if(localStorage.foto !== undefined) {
+			$scope.imagemBd = localStorage.foto;
+			console.log($scope.imagemBd);
+			console.log("deu certo");
+		}
+		else {
+			console.log("sem retorno do bd");
+		}
 	}
 
 
 	$scope.salvarImagens = function() {
-
-		$scope.dadosImagem = ({
-			"imagem": $scope.imageCamera,
-			"tipoImagem": $scope.folder,
-			"idVistoria": dados.usuario.idVistoria
-		});
-
-    localStorage.setItem($scope.folder, $scope.imageCamera);
-		// dados.updateImagens($scope.dadosImagem)
-		// 	.success(function (data, status) {
-		// 		console.log(data);
-		// 		console.log("imagem salvou");
-		// 		$scope.buscaImagemBd();
-		// 	})
-		// 	.error(function (data, status, headers, config) {
-		// 		 console.log(data,  status);
-		// 		 console.log("imagem não salvou");
-		// 	});
+    localStorage.setItem("foto", $scope.imageCamera);
+    $scope.buscaImagemBd();
 	}
 })
